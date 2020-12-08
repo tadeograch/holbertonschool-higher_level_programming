5a2311925e3a085cc41960ffe2cb1077bf22b305
Tadeo Grach <tade.g.17@gmail.com>
2020-12-08 17:12:07 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-print("00", end="")
-for i in range(1, 100):
+print("01", end="")
+for i in range(2, 100):
     if i / 10 > i % 10:
         continue
     print(", {:02d}".format(i), end="")
