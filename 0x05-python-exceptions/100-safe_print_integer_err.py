9257fc2605f82863cf47c8529415cfb48a710378
Tadeo Grach <tade.g.17@gmail.com>
2021-01-04 20:08:35 +0000

    task 100 advanced

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        print("Exception: {0}".format(e), file=sys.stder)
+        print("Exception: {0}".format(e), file=sys.stderr)
         return False
     return True
