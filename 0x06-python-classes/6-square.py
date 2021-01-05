3607571dfc33505b3e0d5df8c493dc3de8ed36fa
Tadeo Grach <tade.g.17@gmail.com>
2021-01-05 20:03:47 +0000

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''Square class'''
+
+
+class Square:
+    '''size and position instantiation'''
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    '''Size getter'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''Size setter'''
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    '''Position getter'''
+    @property
+    def position(self):
+        return self.__position
+
+    '''Position Setter'''
+    @position.setter
+    def position(self, value):
+        if len(value) > 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not type(value[0]) is int) or (not type(value[1]) is int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    '''Returns the area of the square'''
+    def area(self):
+        return self.__size ** 2
+
+    '''Prints the square'''
+    def my_print(self):
+        if self.__position[1] > 0:
+            for l in range(0, self.__position[1]):
+                print()
+        if self.__size > 0:
+            for i in range(0, self.__size):
+                if self.__position[0] > 0:
+                    for k in range(0, self.__position[0]):
+                        print(" ", end="")
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
