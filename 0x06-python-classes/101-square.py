d6000c41b4ce5cb7cbcc530468d0af373caa3075
Tadeo Grach <tade.g.17@gmail.com>
2021-01-05 23:12:19 +0000

    fixing task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -75,12 +75,11 @@ class Square:
     def __str__(self):
         sqr = ""
         if self.__size == 0:
-            sqr = "\n"
+            sqr += "\n"
             return sqr
         if self.__position[1] > 0:
             for l in range(0, self.__position[1]):
                 sqr += "\n"
-
         for i in range(0, self.__size):
             if self.__position[0] > 0:
                 for k in range(0, self.__position[0]):
