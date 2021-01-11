0ee71e334b7698c7d93cd34df7ea420c05dad6b5
Tadeo Grach <tade.g.17@gmail.com>
2021-01-11 22:05:35 +0000

    fixing task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -70,4 +70,4 @@ class Rectangle:
 
     def __repr__(self):
         '''return a string representation of the rectangle'''
-        return "Rectangle({},{})".format(self.__width, self.__height)
+        return "Rectangle({},{})".format(self.width, self.height)
