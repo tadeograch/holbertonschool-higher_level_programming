fd3c6ba3e271a5a8c21655df090baf2c18648ccf
Tadeo Grach <tade.g.17@gmail.com>
2021-01-11 22:08:16 +0000

    fixing task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,6 +10,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''width and height instantiation'''
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
         Rectangle.number_of_instances += 1
