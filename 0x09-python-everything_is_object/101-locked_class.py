fa7438f67b6cb737e51adb00cc7dff5e09118f8f
Tadeo Grach <tade.g.17@gmail.com>
2021-01-12 21:35:30 +0000

    adding task 31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+'''a class LockedClass with no class or object attribute'''
 
 
 class LockedClass():
+    '''prevents the user from dynamically creating new instance attributes'''
     __slots__ = ('first_name',)
