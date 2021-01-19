1b4962cdfbfd4b8c4ffa37ba955dfd2288cbb01d
Tadeo Grach <tade.g.17@gmail.com>
2021-01-19 23:10:58 +0000

    adding task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,4 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         '''Return string'''
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
