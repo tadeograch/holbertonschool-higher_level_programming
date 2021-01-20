9fce1ebf9353613e441ae56b309d85156096d866
Tadeo Grach <tade.g.17@gmail.com>
2021-01-20 23:05:00 +0000

    adding task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,17 +12,16 @@ def pascal_triangle(n):
         return r_list
     a_list = []
     for line in range(0, n):
-        a_list = []  
+        a_list = []
         for i in range(0, line + 1):
-            
             res = 1
             k = i
             j = line
-            if (k > j - k) : 
-                k = j - k 
-            for l in range(0 , k) : 
-                res = res * (j - l) 
-                res = res // (l + 1) 
+            if (k > j - k):
+                k = j - k
+            for l in range(0, k):
+                res = res * (j - l)
+                res = res // (l + 1)
             a_list.append(res)
         r_list.append(list(a_list))
     return r_list
