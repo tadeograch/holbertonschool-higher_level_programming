6538d30012e371dc20f208561be3decc63b69616
Tadeo Grach <tade.g.17@gmail.com>
2021-01-27 13:10:40 +0000

    fixing rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,6 @@ class Rectangle(Base):
     '''Class Rectangle that inherits from base'''
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Instantation of attributes'''
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        super().__init__(id)
         if not type(width) is int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -28,7 +23,11 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
-        
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
