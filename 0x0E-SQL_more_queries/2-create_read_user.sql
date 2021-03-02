c21b27688a736f3ce76186f9981dff0e9c1168b3
Tadeo Grach <tade.g.17@gmail.com>
2021-03-02 17:16:01 +0000

    fixing task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0c_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'hbtn_0c_2' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON *.* TO 'user_0d_2'@'hbtn_0c_2';
+FLUSH PRIVILEGES;
