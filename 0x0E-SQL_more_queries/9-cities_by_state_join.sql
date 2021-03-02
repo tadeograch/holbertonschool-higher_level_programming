0b12770f2b7efe1b12997e5bc33f11c7665cfa85
Tadeo Grach <tade.g.17@gmail.com>
2021-03-02 19:53:41 +0000

    fixing task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 --  Lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id cities.name states.name FROM cities NATURAL JOIN states ORDER BY id;
+SELECT cities.id cities.name states.name FROM cities INNER JOIN states on cities.state_id=states.id ORDER BY id;
