e8ede477a6a12bbe393f1242830c1cbe933249dd
Tadeo Grach <tade.g.17@gmail.com>
2021-03-02 17:12:42 +0000

    adding task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'hbtn_0c_2' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0c_2 TO 'user_0d_2'@'hbtn_0c_2';
