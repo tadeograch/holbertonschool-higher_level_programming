3a8f08eabc0ffba314008096b7e8b5e9b5b7e8f3
Tadeo Grach <tade.g.17@gmail.com>
2021-03-24 00:05:29 +0000

    adding task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
