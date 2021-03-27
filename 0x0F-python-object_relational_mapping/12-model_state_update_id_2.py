95fe2e42b18ccc951800786f2a0441864a4f0a9e
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:48:37 +0000

    fixing task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""""changes the name of a State object from the database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
