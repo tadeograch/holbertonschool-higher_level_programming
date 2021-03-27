8be9ad3ff62efe5295f3bae248f63fd92ec96379
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:02:22 +0000

    fixing task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-'''Lists all states from the database hbtn_0e_0_usa'''
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-MY_HOST = "localhost"
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-
 if __name__ == "__main__":
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
     db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS,
                          db=MY_DB, port=3306, charset="utf8")
     cur = db.cursor()
