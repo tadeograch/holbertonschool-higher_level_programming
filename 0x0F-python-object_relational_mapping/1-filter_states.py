a0539f765665a1f45b2d735ce7be591423b67b71
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:06:16 +0000

    fixing task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-
+"""Lists all states with a name starting with N"""
 import MySQLdb
 import sys
 
-MY_HOST = "localhost"
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
-             ORDER BY states.id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
 if __name__ == "__main__":
-    pass
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
+                ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+    cur.close()
