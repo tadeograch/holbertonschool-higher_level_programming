a4248bac5e72a60cad34c7c62053cbb80248c638
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:42:46 +0000

    fixing task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
