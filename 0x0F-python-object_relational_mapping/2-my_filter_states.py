e6ab0521148ee95e2c0065de2c09e0f44c10b8b1
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:20:29 +0000

    fixing task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
                 ORDER BY states.id".format(MY_NAME))
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
     db.close()
     cur.close()
