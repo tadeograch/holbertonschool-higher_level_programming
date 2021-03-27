6edac076ab211e36a4d41a17c611a76639152c60
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:25:43 +0000

    fixing task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-
+"""Takes in arguments and displays all values in the states table"""
 import MySQLdb
 import sys
 
-MY_HOST = "localhost"
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-MY_NAME = sys.argv[4]
-
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE states.name = %s \
-             ORDER BY states.id", (MY_NAME,))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
 if __name__ == "__main__":
-    pass
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    MY_NAME = sys.argv[4]
+
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS,
+                         db=MY_DB, port=3306, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name = %s \
+                ORDER BY states.id", (MY_NAME,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+    cur.close()
