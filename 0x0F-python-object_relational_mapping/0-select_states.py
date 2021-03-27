45d1de43e5f160977901c57d5d2671175f5ebf4e
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 04:51:09 +0000

    fixing task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,13 @@ MY_USER = sys.argv[1]
 MY_PASS = sys.argv[2]
 MY_DB = sys.argv[3]
 
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
 if __name__ == "__main__":
-    pass
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
