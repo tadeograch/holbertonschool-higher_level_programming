3399d04033db45e8b63fd3154d4a88ea2b764793
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:28:52 +0000

    fixing task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-
+"""Lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
-MY_HOST = "localhost"
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-
-db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-cur = db.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-             INNER JOIN states on cities.state_id=states.id ORDER BY id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
 if __name__ == "__main__":
-    pass
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS,
+                         db=MY_DB, port=3306, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                INNER JOIN states on cities.state_id=states.id ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+    cur.close()
