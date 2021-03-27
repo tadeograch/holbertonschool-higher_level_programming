399993ac1e4543b3db88b30d1525a6f41bd83191
Tadeo Grach <tade.g.17@gmail.com>
2021-03-27 05:41:05 +0000

    fixing task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+lists all State objects that contain the letter a from database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
