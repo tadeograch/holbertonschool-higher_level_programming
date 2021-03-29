dd2ac9c19b260dc3ee9cca4ba152d3bf1caed743
Tadeo Grach <tade.g.17@gmail.com>
2021-03-29 02:14:12 +0000

    adding task 17 advanced

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for s in session.query(State).order_by(State.id):
-        #print("{}: {}".format(s.id, s.name))
         for c in s.cities:
             print("{}: {} -> {}".format(c.id, c.name, s.name))
     session.close()
