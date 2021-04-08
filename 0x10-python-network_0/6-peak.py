6eacb2a8789dc1f6a034e4ec91652b23f5283029
Tadeo Grach <tade.g.17@gmail.com>
2021-04-08 19:30:46 +0000

    adding task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,16 +8,17 @@ def find_peak(list_of_integers):
     n = len(list_of_integers)
     if n == 0:
         return None
-    half = int(n / 2)
-    if list_of_integers[half] < list_of_integers[half - 1]:
-        for i in range(1, half):
-            if list_of_integers[i] >= list_of_integers[i - 1]:
-                if list_of_integers[i] >= list_of_integers[i + 1]:
-                    return list_of_integers[i]
-    elif list_of_integers[half] < list_of_integers[half + 1]:
-        for i in range(half + 1, n):
-            if list_of_integers[i] >= list_of_integers[i - 1]:
-                if list_of_integers[i] >= list_of_integers[i + 1]:
-                    return list_of_integers[i]
+    mid = int(n / 2)
+    return aux_func(list_of_integers, mid)
+
+def aux_func(list_i, n):
+    '''Recursive aux function'''
+    if n < 2:
+        return list_i[n]
+
+    if list_i[n] < list_i[n - 1]:
+        return aux_func(list_i, int(n / 2))
+    elif list_i[n] < list_i[n + 1]:
+        return aux_func(list_i, n + int(n / 2))
     else:
-        return list_of_integers[half]
+        return list_i[n]
