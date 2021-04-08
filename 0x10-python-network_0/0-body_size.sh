c996619b393b6628a90c68150179b8209bb96f52
Tadeo Grach <tade.g.17@gmail.com>
2021-04-08 15:39:08 +0000

    fixing task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI $1 | grep -i Content-Length
+curl -s $1 | grep -i Content-Length
