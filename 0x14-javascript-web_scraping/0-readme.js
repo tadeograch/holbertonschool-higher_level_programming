0bdb1d71684f73b8ac42a89d0be2f31487e46344
Tadeo Grach <tade.g.17@gmail.com>
2021-05-04 16:17:53 +0000

    fixing task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,5 +5,9 @@ const fs = require('fs');
 fs.readFile(process.argv[2], bar);
 
 function bar (err, data) {
-  err ? Function("error","throw error")(err) : console.log(data.toString('utf8') );
-};
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString('utf8'));
+  }
+}
