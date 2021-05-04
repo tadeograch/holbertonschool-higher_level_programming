4d14132544b519a53262a249f34c3326a5b37079
Tadeo Grach <tade.g.17@gmail.com>
2021-05-04 18:15:16 +0000

    fixing task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const results = JSON.parse(body).results;
+    let result = JSON.parse(body).results;
     let num = 0;
-    for (let i = 0; i < results.lenght; i++) {
-      for (let j = 0; i < results[i].characters; j++) {
-        if (results[i].characters[j] === character) {
+    for (let i = 0; i < result.lenght; i++) {
+      let character = result[i].characters;
+      for (let j = 0; j < character.lenght; j++) {
+        if (character[j].includes('18')) {
           num++;
         }
       }
